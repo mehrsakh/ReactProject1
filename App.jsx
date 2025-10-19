e653d5025edf463940adcbe9479d4320c4822348
mehrsakh <mehrsakhalaj74@gmail.com>
2025-10-19 10:40:04 +0330

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+
+import  Header from "../Components/Header.jsx";
+import Footer from '../Components/Footer.jsx';
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+        <h2 style={{ textAlign: 'center', padding: '350px 20px' , color:'#0f172a' }}>
+        <h2>Welcome to My First React App</h2>
+      </h2>
+    <Footer />
+    </>
+  )
+}
+
+export default App ;
